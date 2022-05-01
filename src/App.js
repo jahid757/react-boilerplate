8effe92b7287e705c6e4e2f0b4b5c8ed739f86cd
Jahid <jahidhasan.developer@gmail.com>
2022-05-01 22:53:28 +0600

    React Boilerplate

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/form" element={<Form />} />
         <Route path="*" element={<NotFound />} />
